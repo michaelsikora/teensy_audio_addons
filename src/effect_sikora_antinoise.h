02c38fc4dfa0e178fc3d5b53b716ff7a758dc820
michaelsikora <sikoraaudio@gmail.com>
2022-06-04 23:54:58 +0200

    update copyright

--- src/effect_sikora_antinoise.h
+++ src/effect_sikora_antinoise.h
@@ -1,5 +1,5 @@
-/* Audio Library for Teensy 3.X
- * Copyright (c) 2014, Pete (El Supremo)
+/* Sikora Teensy Audio Addons Library
+ * Copyright (c) 2022, Michael Sikora <sikoraaudio@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
